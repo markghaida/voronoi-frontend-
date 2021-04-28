e08e58a88c9e347191c285da2428162ae7aee41b
joe <josephghaida@gmail.com>
2021-04-28 18:33:48 -0400

    bksp smaller

--- src/components/Keys.jsx
+++ src/components/Keys.jsx
@@ -8,8 +8,8 @@ const keysLayout = {
     '0 1 2 3 4 5 6 7 8 9',
     'q w e r t y u i o p',
     'a s d f g h j k l',
-    'z x c v b n m',
-    '{space} {bksp}'
+    'z x c v b n m {bksp}',
+    '{space}'
   ],
 };
 
