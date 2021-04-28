2f17ad56c1636bdf89a8b1b12763ebb55eb31955
joe <josephghaida@gmail.com>
2021-04-27 22:51:50 -0400

    ditto

--- src/components/Rhizom.js
+++ src/components/Rhizom.js
@@ -43,8 +43,7 @@ const Rhizom = ( { bookmarks } ) => {
 
     let width = rhiz.current.clientWidth;
     let height = rhiz.current.clientHeight;
-    height = rhiz.current.clientHeight;
-
+    height = window.innerHeight;
 
     const delaunay = Delaunay.from( plottedPts );
     const voronoi = delaunay.voronoi( [ 5, 5, width - 5, height - 5 ] );
