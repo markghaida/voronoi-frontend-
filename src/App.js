738a01b0d803eaa6ea9e1fe68670bb2f6bec6c4a
Mark Ghaida <markghaida@gmail.com>
2021-07-05 20:17:35 -0400

    adjusted handling similar_bookmarks from backend using state variables

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App( ){
     }
     let res = searchValue.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
     if( res !== null ){ 
-      console.log("fetching this bookmark now")
+      console.log("saving this bookmark now")
       fetch(backend, {
       method: 'POST',
       headers: {
@@ -32,20 +32,24 @@ function App( ){
 
         // I have to first determine if the data returned is coming
         //back as an error
+        setAdjList(data)
         setSearch("")
         if(data[0] === "Url has already been taken"){
           // setErrors( data[ 0 ] )
         }
       })
     }else{
-      console.log("filtering based off of your input")
+      console.log("filtering based off of your search")
+      // debugger 
       fetch( `${ backend }/search`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify( { search: searchValue } )
+        body: JSON.stringify( { search: searchValue,  objList: adjList} )
         } )
       .then( response => response.json( ) )
       .then( response => {  setLastReceipt( response.search ); filteredList( response.bookmarks ); } );
+      // .then( response => console.log(response));
+      // debugger 
     }
   }
   
@@ -58,6 +62,7 @@ function App( ){
   const [ lastReceipt, setLastReceipt ] = useState( "" );
   const [ searchValue, setSearch ] = useState( "" );
   const [ bookmarks, setBookmarks ] = useState( [ ] );
+  const [ adjList, setAdjList ] = useState ( {} );
   useEffect( ( ) => {
     getBookmarks( );
   }, [ searchValue ] );
